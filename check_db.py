09523e5012e0b6856ac5b90dc4380ae6decba754
leiaway137 <vincent.lei@globalfold.com>
2026-05-01 18:01:31 +0800

    chore: remove check_db.py

--- check_db.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from main import User, SQLALCHEMY_DATABASE_URL
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-with Session(engine) as db:
-    users = db.query(User).all()
-    print("ALL USERS:")
-    for u in users:
-        print(f"ID: {u.id}, Username: '{u.username}', Admin: {u.is_admin}")
